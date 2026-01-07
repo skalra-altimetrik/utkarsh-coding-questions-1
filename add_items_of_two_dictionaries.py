8ccbe38d119ae70471a6773056892911f25946d5
skalra-altimetrik <skalra@altimetrik.com>
2026-01-07 16:25:57 +0530

    updated comments

--- add_items_of_two_dictionaries.py
+++ add_items_of_two_dictionaries.py
@@ -1,3 +1,5 @@
+#problem: Add two dictionary items
+
 dict_1 = {0:2, 1:7}
 dict_2 = {0:5, 1:2, 2:4, 3:8, 4:9, 5:3}
 sum_dict = {}
@@ -5,3 +7,5 @@ for key in set(dict_1)|set(dict_2):
     sum_dict[key] = dict_1.get(key,0)+dict_2.get(key, 0)
  
 print(sum_dict)
+
+#Rating on code - 4/5, Candidate completed the assignment and the expected output was produced.
